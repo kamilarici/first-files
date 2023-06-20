6f1fd98d875279f6c15ebe33bb2fbee2dc82c832
KamilArici <kamilarici5@gmail.com>
2023-06-20 18:07:35 +0300

    react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import Content from "./Content";
+import Footer from "./Footer";
+import Header from "./Header";
+
 function App() {
-  return <div></div>;
+  // js alanı
+  return (
+    // jsx alanı bu alanda jsx formatında yazmak gerekiyor
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <header className="bg-danger">hello react</header>;
+}
+
+export default Header;
